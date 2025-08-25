1561e954564a04198052a65378ae4b8847fc7746
unknown <nikhilkachi68@gmail.com>
2025-08-26 02:10:56 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import './App.css'
-import Banner from './components/Banner'
-import ParallaxScene from './components/ParallaxScene'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Banner from "./components/Banner";
+import ParallaxScene from "./components/ParallaxScene";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    // When all assets (images, css, fonts) are loaded
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
   return (
     <>
-      {/* <ParallaxScene /> */}
-      <Banner />
-
+      {loading ? <Loader /> : <Banner />}
+      {/* <ParallaxScene /> can also go here after loader */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
