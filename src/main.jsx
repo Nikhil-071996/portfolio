b9488ee2bc45bd61693c357488a6d8a464040a62
unknown <nikhilkachi68@gmail.com>
2025-11-23 02:17:00 +0530

    fixed ui

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToastContainer>
       <App />
-    </ToastContainer>
+    <ToastContainer />
   </StrictMode>,
 )
