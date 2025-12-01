2e8c131d8e966678c225cfc2ff7b8c8787c1d5b9
unknown <nikhilkachi68@gmail.com>
2025-12-01 17:29:00 +0530

    change api address

--- src/components/portfolio/PortfolioCards.jsx
+++ src/components/portfolio/PortfolioCards.jsx
@@ -5,87 +5,6 @@ import axios from "axios";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// const portfolioData = [
-//   {
-//     id: 1,
-//     title: "Movie Website",
-//     author: "Pentaclay",
-//     img: "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdeP5qG5X8AJlHz3vwmMk4YgiTpqQtg7Kddhe8E3xO8riQjeShkpK-D7av2n8LRCtNnge_12-WhSh6gHRf5h32LI5aFciDJcHegUI4P3mO6N8a68GpzZX0nM3kTcHGdANeTXQlhqmcBjZEabCootn4lLT4?key=9dbtyPrz-WAw8-HYTpZTqA",
-//     likes: 188,
-//     views: "29.1k",
-//     style: {
-//       background: "linear-gradient(to bottom, #1a1e26, #2c2f38)",
-//       boxShadow: "0 4px 15px rgba(0, 0, 0, 0.6)",
-//       borderRadius: "12px",
-//     },
-//   },
-//   {
-//     id: 2,
-//     title: "Admin Panel",
-//     author: "Orizon Agency",
-//     img: "https://www.superoffice.com/globalassets/home-com-website/content/dashboards/business-insights.png?v=49cbc3",
-//     likes: 154,
-//     views: "44.9k",
-//     style: {
-//       background: "linear-gradient(to bottom, #f7f8f9, #e2e5e8)",
-//       boxShadow: "0 3px 12px rgba(0, 0, 0, 0.1)",
-//       borderRadius: "12px",
-//     },
-//   },
-//   {
-//     id: 3,
-//     title: "E-commerce",
-//     author: "Tommaso Tavormina",
-//     img: "https://cdn.dribbble.com/userupload/13606666/file/original-17521e86823d09eec75a072b7cacd1cd.png?format=webp&resize=400x300&vertical=center",
-//     likes: 93,
-//     views: "15.3k",
-//     style: {
-//       background: "linear-gradient(135deg, #fddde6, #d1fddc)",
-//       boxShadow: "0 6px 20px rgba(0, 0, 0, 0.15)",
-//       borderRadius: "12px",
-//     },
-//   },
-//   {
-//     id: 4,
-//     title: "Movie Website",
-//     author: "Pentaclay",
-//     img: "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdeP5qG5X8AJlHz3vwmMk4YgiTpqQtg7Kddhe8E3xO8riQjeShkpK-D7av2n8LRCtNnge_12-WhSh6gHRf5h32LI5aFciDJcHegUI4P3mO6N8a68GpzZX0nM3kTcHGdANeTXQlhqmcBjZEabCootn4lLT4?key=9dbtyPrz-WAw8-HYTpZTqA",
-//     likes: 188,
-//     views: "29.1k",
-//     style: {
-//       background: "linear-gradient(to bottom, #1a1e26, #2c2f38)",
-//       boxShadow: "0 4px 15px rgba(0, 0, 0, 0.6)",
-//       borderRadius: "12px",
-//     },
-//   },
-//   {
-//     id: 5,
-//     title: "Admin Panel",
-//     author: "Orizon Agency",
-//     img: "https://www.superoffice.com/globalassets/home-com-website/content/dashboards/business-insights.png?v=49cbc3",
-//     likes: 154,
-//     views: "44.9k",
-//     style: {
-//       background: "linear-gradient(to bottom, #f7f8f9, #e2e5e8)",
-//       boxShadow: "0 3px 12px rgba(0, 0, 0, 0.1)",
-//       borderRadius: "12px",
-//     },
-//   },
-//   {
-//     id: 6,
-//     title: "E-commerce",
-//     author: "Tommaso Tavormina",
-//     img: "https://cdn.dribbble.com/userupload/13606666/file/original-17521e86823d09eec75a072b7cacd1cd.png?format=webp&resize=400x300&vertical=center",
-//     likes: 93,
-//     views: "15.3k",
-//     style: {
-//       background: "linear-gradient(135deg, #fddde6, #d1fddc)",
-//       boxShadow: "0 6px 20px rgba(0, 0, 0, 0.15)",
-//       borderRadius: "12px",
-//     },
-//   },
-// ];
-
 export default function PortfolioCards() {
   const cardsRef = useRef([]);
   const [projects, setProjects] = useState([]);
@@ -93,7 +12,7 @@ export default function PortfolioCards() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const res = await axios.get("http://localhost:4000/api/projects");
+        const res = await axios.get(`${import.meta.env.VITE_BASE_URL}projects`);
         setProjects(res.data);
       } catch (err) {
         console.error("Error loading projects", err);
